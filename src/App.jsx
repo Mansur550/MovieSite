b971389948445589d9777cad1632557162650e65
Mansur550 <mansurislam59@gmail.com>
2025-12-10 02:04:47 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const App = () => {
+    return (
+        <><div className='pattern'>
+            <h1></h1>
+        </div>
+            <div className='wrapper'>
+                <header>
+                    <h1>Find Movies You Will Enjoy Without the Hassle</h1>
+                </header>
+
+            </div></>
+    )
+}
+
+export default App
