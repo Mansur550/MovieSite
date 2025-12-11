d195e11c147ce0534eda7848b1960e5641d92f28
Mansur550 <mansurislam59@gmail.com>
2025-12-12 03:39:56 +0600

    movies

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,22 @@ const App = () => {
                 <section className='all-movies'>
                     <h2>All Movies</h2>
 
-                    {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+                    {isLoading ? (
+                        <p className='text-white'>Loading...</p>
+                    ) : errorMessage ? (
+                        <p className='text-red-500'>{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {
+                                movieList.map((movie) => (
+                                    <p key={movie.id} className='text-white'>{movie.title}</p>
+                                ))
+                            }
+                        </ul>
+                    )
+
+
+                    }
 
                 </section>
 
