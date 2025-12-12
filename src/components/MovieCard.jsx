9041af35eb8f35f2fe114e2e8571e6284d617d56
Mansur550 <mansurislam59@gmail.com>
2025-12-13 01:06:05 +0600

    rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MovieCard = ({ movie:
-    { title, vote_agerage, poster_path, release_date, original_language } }) => {
+    { title, vote_average, poster_path, release_date, original_language } }) => {
     return (
         <div className='movie-card'>
             <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` :
@@ -12,6 +12,15 @@ const MovieCard = ({ movie:
 
             <div className='mt-4'>
                 <h3>{title}</h3>
+
+                <div className='content'>
+                    <div className='rating'>
+                        <img src="star.svg" alt="star Icon" />
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+
+                    </div>
+
+                </div>
 
             </div>
 
